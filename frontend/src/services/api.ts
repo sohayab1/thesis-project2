7211ff246aacc757ede5b02b7937cab02758004f
sohayab1 <md.ibna.sohayab@g.bracu.ac.bd>
2025-09-22 00:43:29 +0600

    deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,12 +3,22 @@ import { UserDto, ComplaintDto, DepartmentDto, FeedbackDto } from "@/types/dto";
 import { User } from '@/types/user';
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://localhost:8080/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   withCredentials: true // Add this line
+// });
+
+
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true // Add this line
+  withCredentials: true
 });
 
 
